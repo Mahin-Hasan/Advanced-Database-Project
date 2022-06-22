1fb0ec5a3e5dc91e7d2ebeeec1d4907786529e81
Mahin Hasan <mahinmtrs@gmail.com>
2022-06-22 23:20:25 +0600

    Add files via upload

---
+++ Employee_Registration/src/employee_registration/Employee_Registration.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee_registration;
+
+/**
+ *
+ * @author new
+ */
+public class Employee_Registration {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
